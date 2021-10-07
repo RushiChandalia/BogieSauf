3ac0d9ad2a51197735ec98e29f16de67e3a4bd1a
RushiChandalia <rushichandalia@gmail.com>
2021-10-07 18:15:24 +0530

    zoom in acheivenments

---
+++ src/Components/Achievements/Carousel/CarouselModal/CarouselModal.jsx
@@ -0,0 +1,68 @@
+import * as React from "react";
+import Modal from "@mui/material/Modal";
+import RP from "../../../../Images/RP.jpg";
+import DNA from "../../../../Images/DNA.jpg";
+import Divya from "../../../../Images/Divya.jpg";
+import StartUp from "./Achievements_certi/startupgrind.jpeg"
+import Makers1 from "./Achievements_certi/makers.jpeg"
+import Makers2 from "./Achievements_certi/maker2.jpeg"
+
+import "./carouselDesktop.css"
+export default function CarouselModal({ open, handleClose, state }) {
+  switch (state) {
+    case "RP":
+      return (
+        <div>
+          <Modal open={open} onClose={handleClose} className="modal-wrapper">
+            <div className="modal-content">
+              <img src={RP} id="newspaper_articles" className={`newspaper_${state}`} alt="" />
+            </div>
+          </Modal>
+        </div>
+      );
+    case "DNA":
+      return (
+        <div>
+          <Modal open={open} onClose={handleClose} className="modal-wrapper">
+            <div className="modal-content">
+              <img src={DNA} id="newspaper_articles" className={`newspaper_${state}`} alt="" />
+            </div>
+          </Modal>
+        </div>
+      );
+    case "Divya":
+      return (
+        <div>
+          <Modal open={open} onClose={handleClose} className="modal-wrapper">
+            <div className="modal-content">
+              <img src={Divya} id="newspaper_articles" className={`newspaper_${state}`} alt="" />
+            </div>
+          </Modal>
+        </div>
+      );
+    case "Pitchbattle":
+      return (
+        <div>
+          <Modal open={open} onClose={handleClose} className="modal-wrapper">
+            <div className="modal-content">
+              <img src={StartUp} id="Compi_certi" className={`compi_${state}`} alt="" />
+            </div>
+          </Modal>
+        </div>
+      );
+    case "Maker Fest Vadodara 2021":
+      return (
+        <div>
+          <Modal open={open} onClose={handleClose} className="modal-wrapper" >
+            <div className="makers">
+              <img src={Makers1} id="Compi_certi" className={`compi_makers`} alt="" />
+              <img src={Makers2} id="Compi_certi" className={`compi_makers`} alt="" />
+            </div>
+          </Modal>
+        </div>
+      );
+
+    default:
+      return <h1>default</h1>
+  }
+}
